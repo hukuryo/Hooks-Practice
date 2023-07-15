485850211d607883d772da9243dc11d1e127d3f8
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-15 10:34:51 +0900

    done

--- hooks-practice/app/page.tsx
+++ hooks-practice/app/page.tsx
@@ -1,30 +1,37 @@
 "use client";
-import React, { useCallback } from 'react';
-import Image from 'next/image'
+import React, { useCallback, useEffect } from 'react';
 import { useState } from 'react'
 
 export default function Home() {
+  
+  // useState
   const [count, setCount] = useState(0);
-  const onClickCountUp = useCallback(() => {
+  const onClickCountUp = () => {
     setCount(count + 1);
-  }, [count]); 
+  }; 
+  
+    // useEffect
+  useEffect(() => {
+    console.log("カウントアップ")
+  }, [])
+  
+  // useCallback
   const [count2, setCount2] = useState(0);
   const onClickCountUp2 = useCallback(() => {
     setCount2(count2 + 1);
   }, [count]); 
 
+
   return (
     <>
       <div className='text-center mt-10'>
-        <h2 className='font-bold'>useState</h2>
+        <h2 className='font-bold'>useState、useEffect</h2>
         <p className='mb-10'>{count}</p>
         <button className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mb-10" onClick={onClickCountUp}>カウントアップ</button>
-        <h2 className='font-bold mb-10'>useCallback</h2>
+        {/* <h2 className='font-bold mb-10'>useCallback</h2>
         <p className='mb-10'>{count2}</p>
         <button className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mb-10" onClick={onClickCountUp2}>カウントアップ</button>
-        <hr />
-      </div>
-      <div className='text-center mt-10'>
+        <hr /> */}
       </div>
     </>
   )
