2a2411e762900fe89b6a1f00a3f927dcf8fc56b8
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-20 23:10:50 +0900

    useMemoを書いた

--- hooks-practice/app/components/Practice.tsx
+++ hooks-practice/app/components/Practice.tsx
@@ -1,8 +1,10 @@
-import { useCallback, useState } from "react"
+import { useCallback, useState, useContext } from "react"
+
+
 
 type counter = {
     showCount: () => void
-  }
+}
 
 export function Practice(props: counter) {
     return(
--- hooks-practice/app/page.tsx
+++ hooks-practice/app/page.tsx
@@ -1,14 +1,21 @@
 "use client";
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { useState } from 'react'
 import { Practice } from './components/Practice';
+import { createContext } from 'vm';
 
 type counter = {
   showCount: () => void
 }
 
+const text = {
+  name: "hukuryo",
+  age: "25"
+}
+
+const TextContext = createContext(text);
+
 export default function Home() {
-  
   // useState
   const [count, setCount] = useState(0);
   const onClickCountUp = () => {
@@ -27,17 +34,26 @@ export default function Home() {
     alert('コールバック')
   }, [counter])
 
+  const [count1,setCount1] = useState(0);
+  const [count2,setCount2] = useState(0);
+  const countUp = (count1: number) => {
+    count1 * 2
+  };
+  const baiCount = useMemo(() => countUp(count1), [count1]);
+
   return (
-    <>
-      <div className='text-center mt-10'>
-        <h2 className='font-bold'>useState、useEffect</h2>
-        <p className='mb-10'>{count}</p>
-        <button className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mb-10" onClick={onClickCountUp}>カウントアップ</button>
-      </div>
-      <div className='text-center mt-10'>
-      <h2 className='font-bold'>useCallback</h2>
-        <Practice showCount={showCounter}/>
-      </div>
-    </>
+    <TextContext.Provider value={text}>
+      <>
+        <div className='text-center mt-10'>
+          <h2 className='font-bold'>useState、useEffect</h2>
+          <p className='mb-10'>{count}</p>
+          <button className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mb-10" onClick={onClickCountUp}>カウントアップ</button>
+        </div>
+        <div className='text-center mt-10'>
+        <h2 className='font-bold'>useCallback</h2>
+          <Practice showCount={showCounter}/>
+        </div>
+      </>
+    </TextContext.Provider>
   )
 }
