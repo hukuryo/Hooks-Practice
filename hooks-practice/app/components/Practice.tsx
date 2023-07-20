37684bc041b1b3534a0c66900a8eaf941ed1c6a5
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-20 21:24:40 +0900

    Qiita用のuseCallbackを書いた

---
+++ hooks-practice/app/components/Practice.tsx
@@ -0,0 +1,11 @@
+import { useCallback, useState } from "react"
+
+type counter = {
+    showCount: () => void
+  }
+
+export function Practice(props: counter) {
+    return(
+        <div>Practice</div>
+    )
+}
--- hooks-practice/app/page.tsx
+++ hooks-practice/app/page.tsx
@@ -1,26 +1,31 @@
 "use client";
 import React, { useCallback, useEffect } from 'react';
 import { useState } from 'react'
+import { Practice } from './components/Practice';
+
+type counter = {
+  showCount: () => void
+}
 
 export default function Home() {
   
   // useState
   const [count, setCount] = useState(0);
   const onClickCountUp = () => {
+    console.log("カウントアップ")
     setCount(count + 1);
   }; 
   
-    // useEffect
+  // useEffect
   useEffect(() => {
     console.log("カウントアップ")
   }, [])
-  
-  // useCallback
-  const [count2, setCount2] = useState(0);
-  const onClickCountUp2 = useCallback(() => {
-    setCount2(count2 + 1);
-  }, [count]); 
 
+  const [counter, setCounter] = useState(0);
+
+  const showCounter = useCallback(() => {
+    alert('コールバック')
+  }, [counter])
 
   return (
     <>
@@ -28,10 +33,10 @@ export default function Home() {
         <h2 className='font-bold'>useState、useEffect</h2>
         <p className='mb-10'>{count}</p>
         <button className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mb-10" onClick={onClickCountUp}>カウントアップ</button>
-        {/* <h2 className='font-bold mb-10'>useCallback</h2>
-        <p className='mb-10'>{count2}</p>
-        <button className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mb-10" onClick={onClickCountUp2}>カウントアップ</button>
-        <hr /> */}
+      </div>
+      <div className='text-center mt-10'>
+      <h2 className='font-bold'>useCallback</h2>
+        <Practice showCount={showCounter}/>
       </div>
     </>
   )
