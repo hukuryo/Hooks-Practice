8195c0be406557aff48eb4cbd4242f44023e29bc
hukuryo <ryohei.1998.05.09@gmail.com>
2023-07-21 21:11:26 +0900

    Qiitayouno

--- hooks-practice/app/components/Practice.tsx
+++ hooks-practice/app/components/Practice.tsx
@@ -1,3 +1,4 @@
+import { type } from "os"
 import { useCallback, useState, useContext } from "react"
 
 
@@ -6,8 +7,20 @@ type counter = {
     showCount: () => void
 }
 
-export function Practice(props: counter) {
+type practiceText = {
+    name: number;
+    age: number;
+}
+
+export function Practice(props: practiceText) {
+    const { name, age } = props;
     return(
-        <div>Practice</div>
+        <div className="text-center mt-10">
+            <h1 className="mb-10">Practice props</h1>
+            <h4 className="font-bold mb-5">名前：{name}</h4>
+            <p></p>
+            <h4 className="font-bold">年齢：{age}歳</h4>
+            <p></p>
+        </div>
     )
 }
--- hooks-practice/app/page.tsx
+++ hooks-practice/app/page.tsx
@@ -8,12 +8,7 @@ type counter = {
   showCount: () => void
 }
 
-const text = {
-  name: "hukuryo",
-  age: "25"
-}
 
-const TextContext = createContext(text);
 
 export default function Home() {
   // useState
@@ -27,33 +22,30 @@ export default function Home() {
   useEffect(() => {
     console.log("カウントアップ")
   }, [])
-
+  
   const [counter, setCounter] = useState(0);
-
+  
   const showCounter = useCallback(() => {
     alert('コールバック')
   }, [counter])
-
+  
   const [count1,setCount1] = useState(0);
   const [count2,setCount2] = useState(0);
   const countUp = (count1: number) => {
     count1 * 2
   };
   const baiCount = useMemo(() => countUp(count1), [count1]);
-
+  
+  
+  type text = {
+    name: string;
+    age: number;
+  }
+  
+  
   return (
-    <TextContext.Provider value={text}>
-      <>
-        <div className='text-center mt-10'>
-          <h2 className='font-bold'>useState、useEffect</h2>
-          <p className='mb-10'>{count}</p>
-          <button className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mb-10" onClick={onClickCountUp}>カウントアップ</button>
-        </div>
-        <div className='text-center mt-10'>
-        <h2 className='font-bold'>useCallback</h2>
-          <Practice showCount={showCounter}/>
-        </div>
-      </>
-    </TextContext.Provider>
+    <>
+      <Practice name={"hukuryo"} age={25}/>
+    </>
   )
 }
